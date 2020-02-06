c2fd2c67e326e421a885e3b5e7a16e73c3056dad
atopdev <25312831+atopdev@users.noreply.github.com>
2020-02-06 09:19:55 +0800

    integrate api and use hook to fetch image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import './App.scss';
+
+const UnsplashImage = ({ url, key }) => (
+  <div className="image-item" key={key}>
+    <img src={url} alt="myimage" />
+  </div>
+);
 
 function App() {
+  const [images, setImages] = React.useState([]);
+  const [loaded, setIsLoaded] = React.useState(false);
+
+  const fetchImages = (count = 10) => {
+    const apiRoot = 'https://api.unsplash.com';
+    const accessKey =
+      'a22f61e98da4efa25d8860e77a91a596867dd335ecdf7feb12e086943db9565a';
+
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then(res => {
+        setImages([...images, ...res.data]);
+        setIsLoaded(true);
+
+        console.log(images);
+      });
+  };
+
+  React.useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero is-fullheight is-bold is-info">
+      <div className="hero-body">
+        <div className="container">
+          <div className="header content">
+            <h1 className="title is-1">
+              React Image Gallery with Infinite Scroll
+            </h1>
+            <h2 className="subtitle is-6">by atopdev</h2>
+          </div>
+
+          <div className="image-grid" style={{ marginTop: '30px' }}>
+            {loaded
+              ? images.map((image, index) => (
+                  <UnsplashImage url={image.urls.regular} key={index} />
+                ))
+              : ''}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
